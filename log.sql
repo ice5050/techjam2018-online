3bc1ad0c8820c853d98e1f7a6a3db4e37d22ef07
Ice5050 <ice50505050@hotmail.com>
2018-09-13 22:21:00 +0700

    [Added] Amount of purchasing by each category by credit card

--- log.sql
+++ log.sql
@@ -36,3 +36,27 @@ FROM demo, LATERAL (SELECT
 								EXTRACT(MONTH FROM age(date '2018-07-06', account_start_date)) AS diff_month,
 								EXTRACT(DAY FROM age(date '2018-07-06', account_start_date)) AS diff_day
 							) s;
+
+-- Amount of purchasing by each category by credit card
+SELECT
+        y.user_id,
+        COUNT (CASE category WHEN  'cat1' THEN 1 ELSE NULL END) as cat1,
+        COUNT (CASE category WHEN  'cat2' THEN 1 ELSE NULL END) as cat2,
+        COUNT (CASE category WHEN  'cat3' THEN 1 ELSE NULL END) as cat3,
+        COUNT (CASE category WHEN  'cat4' THEN 1 ELSE NULL END) as cat4,
+        COUNT (CASE category WHEN  'cat5' THEN 1 ELSE NULL END) as cat5,
+        COUNT (CASE category WHEN  'cat6' THEN 1 ELSE NULL END) as cat6,
+        COUNT (CASE category WHEN  'cat7' THEN 1 ELSE NULL END) as cat7,
+        COUNT (CASE category WHEN  'cat8' THEN 1 ELSE NULL END) as cat8,
+        COUNT (CASE category WHEN  'cat9' THEN 1 ELSE NULL END) as cat9,
+        COUNT (CASE category WHEN 'cat10' THEN 1 ELSE NULL END) as cat10,
+        COUNT (CASE category WHEN 'cat11' THEN 1 ELSE NULL END) as cat11,
+        COUNT (CASE category WHEN 'cat12' THEN 1 ELSE NULL END) as cat12,
+        COUNT (CASE category WHEN 'cat13' THEN 1 ELSE NULL END) as cat13,
+        COUNT (CASE category WHEN 'cat14' THEN 1 ELSE NULL END) as cat14,
+        COUNT (CASE category WHEN 'cat15' THEN 1 ELSE NULL END) as cat15,
+        COUNT (CASE category WHEN 'cat16' THEN 1 ELSE NULL END) as cat16
+FROM y_train y
+JOIN cc_txn AS cc
+ON y.user_id = cc.user_id
+GROUP BY y.user_id
